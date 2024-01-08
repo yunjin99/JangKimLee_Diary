8a0507f79a660aa21b01a81005c24067263eba43
Jieun <jangjengineer@gmail.com>
2024-01-08 19:30:40 +0900

    edit savememo, savechecklist date

--- src/main/webapp/calendar.js
+++ src/main/webapp/calendar.js
@@ -3,6 +3,8 @@
 })();
 
 var nowDate = new Date();
+var selectedYear, selectedMonth, selectedDay;
+
 function calendarMaker(target, date) {
 	if (date == null || date == undefined) {
 		date = new Date();
@@ -45,7 +47,7 @@ function calendarMaker(target, date) {
 	$(target).find("#custom_set_date").append(tag);
 	calMoveEvtFn();
 
-            
+
 	function assembly(year, month) {
 		var calendar_html_code =
 			"<table class='custom_calendar_table'>" +
@@ -85,15 +87,15 @@ function calendarMaker(target, date) {
 				console.log('서버 응답 데이터:', response);
 				var memoHTML = "<br><table style='width:100%; font-size: 30px;'>";
 				response.forEach(function(memo) {
-                	let id = memo.memoId;
-                	let content = memo.memoContents;
-                	let memoDate =`${year} + "-" + ${month} + "-" + ${day}`;
-                // 예시: title과 content를 화면에 추가
-                	memoHTML += `<tr><td>` + id + `</td><td><input id='memoContents` + id + `' value='`+ content + `' style='font-size:35px; width:100%'></input></td><td><button value=` + id + `onclick='deleteMemo(` + id + `, ` + year + `, ` + month + `, ` + day + `)'>Delete</button></td> <td><button value=` + id + ` onclick='editMemo(value, ${memoDate})'>Edit</button></td> </tr>`;
-            });
-            // 최종적으로 모든 메모를 화면에 표시
-           memoHTML += `</table> <button onclick="saveMemo(${year}, ${month}, ${day})">다이어리 추가</button> 
-			<div id='insertUiMemoView'></div>`; 
+					let id = memo.memoId;
+					let content = memo.memoContents;
+					let memoDate = `${year} + "-" + ${month} + "-" + ${day}`;
+					// 예시: title과 content를 화면에 추가
+					memoHTML += `<tr><td>` + id + `</td><td><input id='memoContents` + id + `' value='` + content + `' style='font-size:35px; width:100%'></input></td><td><button value=` + id + `onclick='deleteMemo(` + id + `, ` + year + `, ` + month + `, ` + day + `)'>Delete</button></td> <td><button value=` + id + ` onclick='editMemo(value, ${memoDate})'>Edit</button></td> </tr>`;
+				});
+				// 최종적으로 모든 메모를 화면에 표시
+				memoHTML += `</table> <button onclick="saveMemo(${year}, ${month}, ${day})">다이어리 추가</button> 
+			<div id='insertUiMemoView'></div>`;
 				updateMemoView(memoHTML);
 			},
 			error: function(error) {
@@ -102,7 +104,7 @@ function calendarMaker(target, date) {
 		});
 	}
 
-	window.fetchChecklistByDate = function(year, month, day){
+	window.fetchChecklistByDate = function(year, month, day) {
 		console.log("fetchMemoByDate 호출 " + year + '-' + month + '-' + day);
 		var apiUrl = 'checklistByDate?date=' + year + '-' + month + '-' + day;
 		// AJAX를 사용하여 API 호출
@@ -118,7 +120,7 @@ function calendarMaker(target, date) {
 					let status = checklist.checkStatus;
 					let id = checklist.checkId;
 					let content = checklist.checkContents;
-					let checklistDate =`${year} + "-" + ${month} + "-" + ${day}`;
+					let checklistDate = `${year} + "-" + ${month} + "-" + ${day}`;
 					checklistHTML += `<tr><td><input id='checkBox` + id + `' type='checkbox' ` + (status ? "checked" : "unchecked") + `></td><td>` + id + `</td><td><input id='checklistContents` + id + `' value='` + content + `' style='font-size:35px'></input></td><td><button id=` + id + ` onclick='deleteChecklist(` + id + `, ` + year + `, ` + month + `, ` + day + `)'>Delete</button></td> <td><button id='` + id + `' onclick='editChecklist(id, ${checklistDate})'>Edit</button></td></tr>`;
 				})
 				// 최종적으로 모든 메모를 화면에 표시
@@ -136,12 +138,12 @@ function calendarMaker(target, date) {
 	function updateMemoView(memoHTML) {
 		document.getElementById("memo").innerHTML = memoHTML;
 	}
-	
-		// 화면 갱신을 담당하는 함수
+
+	// 화면 갱신을 담당하는 함수
 	function updateCheckView(checklistHTML) {
 		document.getElementById("checklist").innerHTML = checklistHTML;
 	}
-	
+
 	function calMoveEvtFn() {
 		//전달 클릭
 		$(".custom_calendar_table").on("click", ".prev", function() {
@@ -157,9 +159,11 @@ function calendarMaker(target, date) {
 		$(".custom_calendar_table").on("click", "td", function() {
 			$(".custom_calendar_table .select_day").removeClass("select_day");
 			$(this).removeClass("select_day").addClass("select_day");
-			date = $(this).text();
-			fetchMemoByDate(year, month, date);
-			fetchChecklistByDate(year, month, date);
+			selectedYear = nowDate.getFullYear();
+			selectedMonth = nowDate.getMonth() + 1;
+			selectedDay = $(this).text();
+			fetchMemoByDate(selectedYear, selectedMonth, selectedDay);
+			fetchChecklistByDate(selectedYear, selectedMonth, selectedDay);
 		});
 	}
 }
